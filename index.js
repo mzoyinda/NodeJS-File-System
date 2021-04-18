0f7934ef78dbb6159bc774761a13474b2e2e0bd8
oyindamola <oyindamoladawodu22@gmail.com>
2021-04-18 05:09:07 +0100

    File system created

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+const url = "http://jsonplaceholder.typicode.com/posts";
+
+//fetch data from url
+
+const getData = async(url) => {
+
+    try {
+        const response = await fetch(url);
+        const Posts = await response.json();
+
+        //write fetched data to a file
+        fs.writeFile('posts.json', JSON.stringify(Posts, null, 2), 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    } catch (error) {
+        console.log(error);
+   }   
+};
+
+getData(url);
